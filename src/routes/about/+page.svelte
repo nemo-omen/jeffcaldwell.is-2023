e1b50bf0d55a13462454d501af867b3bfbb67506
nemo-omen <nemo.omen@gmail.com>
2023-03-01 11:50:55 -0600

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,10 +6,30 @@
 
 <article>
 <p>
-I'm a digital journalist who's in the last year of getting a computer science degree in San Angelo, Texas. I love making websites and writing and I have a personal focus on making sites that are accessible, fast and great looking.
+I'm a digital journalist in the last year of my computer science degree in San Angelo, Texas. I love making websites and writing, and I focus on creating sites that are accessible, fast, and great looking.
 </p>
 
 <p>
-I grew upo in San Angelo but spent the greater part of my adulthood living in Brooklyn, NY. I moved back in 2018 to pursue my degree.
+I grew up in San Angelo but spent most of my adulthood living in Brooklyn, NY, where I worked in the print industry making cool stuff for clients like the _New York Public Library_ and _Last Week Tonight with John Oliver_.
+</p>
+
+<p>
+I moved back to San Angelo in 2018 to pursue my degree and got a job as a camera operator at the local tv news station. It was only a short time before I found my way into the newsroom, first as a news photographer, then as a multimedia journalist.
+</p>
+
+<p>
+Now I lead a small team of digital journalists for the station's website. I'm not out in the field getting news as much as I used to be, but I'm proud of the team we've been able to build, and it's been amazing to see the growth of the journalists who work for me.
+</p>
+
+<p>
+I've been working toward a degree in computer science since 2020 after I used HTML, CSS, and JavaScript to make on-screen graphics for a live stream. Before then, I wanted a degree in communications. However, coding was too much fun, so I changed my major.
+</p>
+
+<p>
+I still love journalism and serving my community, but I would like to make a change. I'm looking for a position as a technical writer or a front-end developer focusing on semantic HTML and accessibility. My experience in journalism, education in computer science, and focus on accessibility allow me to understand complex subjects and present them to people in a way they can immediately understand.
+</p>
+
+<p>
+If you're hiring, take a look at <a href="/resume">my resume</a>, and let's talk.
 </p>
 </article>
